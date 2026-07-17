2b4bfb433ea8500a54658012853e358b04bd309c
Liz_Crellin <ecrell01@student.bbk.ac.uk>
2026-07-17 16:39:20 +0100

    Load chartevents table

---
+++ sql/setup/import/load_chartevents.sql
@@ -0,0 +1,16 @@
+--This script has been adapted from the original MIMIC-IV load_gz.sql script to load individual tables as required.
+
+
+-----------------------------------------
+-- Load data into the MIMIC-IV schemas --
+-----------------------------------------
+
+\cd :mimic_data_dir
+
+-- making sure that all tables are emtpy and correct encoding is defined -utf8- 
+SET CLIENT_ENCODING TO 'utf8';
+
+--icu schema
+\cd icu
+
+\COPY mimiciv_icu.chartevents FROM PROGRAM 'gzip -dc chartevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';

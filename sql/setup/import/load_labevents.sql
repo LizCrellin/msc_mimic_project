4e33a8018a8381c1c7fe8ed3454f8535addb2095
Liz_Crellin <ecrell01@student.bbk.ac.uk>
2026-07-19 16:55:05 +0100

    Updated to import filtered version of labevents

--- sql/setup/import/load_labevents.sql
+++ sql/setup/import/load_labevents.sql
@@ -7,10 +7,12 @@
 
 \cd :mimic_data_dir
 
--- making sure that all tables are emtpy and correct encoding is defined -utf8- 
+-- making sure that all tables are empty and correct encoding is defined -utf8- 
 SET CLIENT_ENCODING TO 'utf8';
 
 -- hosp schema
 \cd hosp
 
-\COPY mimiciv_hosp.labevents FROM PROGRAM 'gzip -dc labevents.csv.gz' DELIMITER ',' CSV HEADER NULL '';
+-- Loading the filtered table into the hosp schema 
+-- Only the retained columns are imported
+\COPY mimiciv_hosp.labevents (labevent_id, subject_id, hadm_id, specimen_id, itemid, order_provider_id, charttime, value, valuenum, valueuom, ref_range_lower, ref_range_upper, flag, priority) FROM 'labevents_filtered.csv' DELIMITER ',' CSV HEADER NULL '';

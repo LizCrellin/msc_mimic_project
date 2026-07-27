ac5bfdcc40581f6950023d78e80fe51c1501baed
Liz_Crellin <ecrell01@student.bbk.ac.uk>
2026-07-27 09:08:22 +0100

    Change to indexes

--- sql/setup/03_create_indexes.sql
+++ sql/setup/03_create_indexes.sql
@@ -5,14 +5,11 @@
 -- Purpose:
 -- Create indexes on the large tables in the MIMIC IV dataset, chart
 -- events and lab events.
--- For chart events this will be on stay_id and charttime to allow 
--- for generation of the concept views and subsequent joining to the spine 
--- on time. 
+-- For chart events this will be a composite index on (itemid, stay_id, charttime) 
+-- as processing starts with filtering on itemid for the features but also has
+-- GROUP BY on stay_id and charttime and later, joins to the spine based on time.
 -- For labevents, this will be on itemid, hadm_id and charttime for the same
 -- reasons. 
--- I have already preselected itemid on import based on itemid
--- required for the project, so don't expect a large benefit from indexing
--- itemid.
 --
 -- TO DO:
 --
@@ -20,7 +17,7 @@
 
 
 CREATE INDEX ix_chartevents_itemid_stay_charttime
-    ON mimiciv_icu.chartevents (stay_id, charttime);
+    ON mimiciv_icu.chartevents (itemid, stay_id, charttime);
 
 CREATE INDEX ix_labevents_itemid_hadm_charttime
-    ON mimiciv_hosp.labevents (hadm_id, charttime);
+    ON mimiciv_hosp.labevents (itemid, hadm_id, charttime);

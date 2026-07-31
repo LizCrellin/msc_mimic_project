8f1386a73d0608f98ae60de9b6a69794d210ae1f
Liz_Crellin <ecrell01@student.bbk.ac.uk>
2026-07-31 17:12:23 +0100

    Variable naming changes, keep additional id variables

--- sql/analysis/project_hourly_data.sql
+++ sql/analysis/project_hourly_data.sql
@@ -11,9 +11,12 @@
 -- All stays are first ICU stays within first hospital stays, therefore
 -- joins can be made on hadm_id as well as stay_id and will stil relate to
 -- the same stay.
+-- Naming of icustay_id and hour variable have been updated to match up 
+-- with publicly available pipelines for ease of adapting their code. Specifically
+-- this one: https://github.com/MLforHealth/MIMIC_Extract/tree/master
 --
 -- TO DO:
--- Later: consider adding intervention events.
+-- Later: consider adding intervention events (ventilation, vasoactive agents)
 --
 -- -----------------------------------------------------------------------------
 
@@ -25,9 +28,10 @@ CREATE VIEW msc_project.hourly_data AS
 with cohort_hours AS
 (
     SELECT
-        p.stay_id,
+        p.stay_id as icustay_id,
+        p.subject_id,
         p.hadm_id,
-        h.hr,
+        h.hours_in,
         h.hour_end
     FROM msc_project.icustay_hourly AS h
     INNER JOIN msc_project.allpatients AS p
@@ -36,8 +40,8 @@ with cohort_hours AS
 vitalsigns_hourly AS
 (
     SELECT
-        ch.stay_id,
-        ch.hr,
+        ch.icustay_id,
+        ch.hours_in,
         AVG(v.heart_rate) AS heart_rate,
         AVG(v.sbp) AS sbp,
         AVG(v.dbp) AS dbp,
@@ -54,13 +58,13 @@ vitalsigns_hourly AS
         ON v.stay_id = ch.stay_id
         AND v.charttime > ch.hour_end - INTERVAL '1' HOUR
         AND v.charttime < ch.hour_end
-    GROUP BY ch.stay_id, ch.hr
+    GROUP BY ch.stay_id, ch.hours_in
 ),
 chemistry_hourly AS
 (
     SELECT
-        ch.stay_id,
-        ch.hr,
+        ch.icustay_id,
+        ch.hours_in,
         avg(c.albumin) AS albumin,
         avg(c.globulin) AS globulin,
         avg(c.total_protein) AS total_protein,
@@ -79,26 +83,26 @@ chemistry_hourly AS
         ON ch.hadm_id = c.hadm_id
         AND c.charttime > ch.hour_end - INTERVAL '1' HOUR
         AND c.charttime < ch.hour_end
-    GROUP BY ch.stay_id, ch.hr
+    GROUP BY ch.stay_id, ch.hours_in
 ),
 gcs_hourly AS
 (
     SELECT
-        ch.stay_id,
-        ch.hr,
+        ch.icustay_id,
+        ch.hours_in,
         avg(g.gcs) as gcs
     FROM cohort_hours as ch
     INNER JOIN mimiciv_derived.gcs AS g
         ON ch.stay_id = g.stay_id
         AND g.charttime > ch.hour_end - INTERVAL '1' HOUR
         AND g.charttime < ch.hour_end
-    GROUP BY ch.stay_id, ch.hr
+    GROUP BY ch.stay_id, ch.hours_in
 ),
 blood_hourly AS 
 (
     SELECT
-        ch.stay_id,
-        ch.hr,
+        ch.icustay_id,
+        ch.hours_in,
         avg(hematocrit) as hematocrit,
         avg(hemoglobin) as hemoglobin,
         avg(mch) as mch,
@@ -114,11 +118,13 @@ blood_hourly AS
         ON ch.hadm_id = b.hadm_id
         AND b.charttime > ch.hour_end - INTERVAL '1' HOUR
         AND b.charttime < ch.hour_end
-    GROUP BY ch.stay_id, ch.hr
+    GROUP BY ch.stay_id, ch.hours_in
 )
 SELECT
-    ch.stay_id,
-    ch.hr,
+    ch.subject_id,
+    ch.hadm_id,
+    ch.icustay_id,
+    ch.hours_in,
     ch.hour_end,
     vh.heart_rate,
     vh.sbp,
@@ -156,14 +162,15 @@ SELECT
     bh.wbc
 FROM cohort_hours as ch
 LEFT JOIN vitalsigns_hourly as vh
-    ON ch.stay_id = vh.stay_id
-    AND ch.hr = vh.hr
+    ON ch.icustay_id = vh.icustay_id
+    AND ch.hours_in = vh.hours_in
 LEFT JOIN chemistry_hourly as chh
-    ON ch.stay_id = chh.stay_id
-    AND ch.hr = chh.hr
+    ON ch.icustay_id = chh.icustay_id
+    AND ch.hours_in = chh.hours_in
 LEFT JOIN gcs_hourly as gh
-    ON ch.stay_id = gh.stay_id
-    AND ch.hr = gh.hr
+    ON ch.icustay_id = gh.icustay_id
+    AND ch.hours_in = gh.hours_in
 LEFT JOIN blood_hourly as bh
-    ON ch.stay_id = bh.stay_id
-    AND ch.hr = bh.hr;
+    ON ch.icustay_id = bh.icustay_id
+    AND ch.hours_in = bh.hours_in
+ORDER BY ch.icustay_id, ch.hours_in;

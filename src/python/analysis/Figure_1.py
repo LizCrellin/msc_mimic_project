dc4097cf2b9b0f147b8ece899c2f0d4b2dce2cc2
Liz_Crellin <ecrell01@student.bbk.ac.uk>
2026-08-22 17:40:37 +0100

    Developing table 1

--- src/python/analysis/Figure_1.py
+++ src/python/analysis/Figure_1.py
@@ -21,6 +21,50 @@ import pandas as pd
 from scipy.stats import ttest_ind_from_stats, spearmanr
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
-%matplotlib inline
+import getpass
+from sqlalchemy import create_engine
+
+pg_user = 'postgres'      # same value you use to connect via psql
+pg_host = 'localhost'          # or wherever your Postgres server is
+pg_port = 5432
+pg_dbname = 'mimiciv'
+
+pg_password = getpass.getpass('Postgres password: ')
+
+engine = create_engine(
+    f'postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_dbname}'
+)
 
 
+# read in the icu stays table with only included patients
+# method as in notebook
+
+icu_stays = pd.read_sql("SELECT * FROM msc_project.allpatients", engine)
+
+print(icu_stays.head())
+print(icu_stays.shape)
+
+
+# Cohort characteristics for LOS >= 7 days versus shorter length of stay
+
+icu_stays['los_7'] = (icu_stays['los_icu'] > 7).astype(int)
+
+# get length of stay in hospital
+icu_stays['los_hosp'] = (icu_stays['dischtime'] - icu_stays['admittime']).dt.days
+print(icu_stays['los_hosp'])
+
+# Two groups, LOS >= 7 and the rest
+group0 = icu_stays[icu_stays['los_7'] == 0]
+group1 = icu_stays[icu_stays['los_7'] == 1]
+
+# continuous variables
+rows = []
+for var in ['admission_age', 'los_icu', 'los_hosp']:
+    rows.append({
+        'variable': var,
+        'los_7 = 0': f'{group0[var].mean()} \u00B1 {group0[var].std()}',
+        'los_7 = 1': f'{group1[var].mean()} \u00B1 {group1[var].std()}'
+    })
+
+table1 = pd.DataFrame(rows)
+print(table1)

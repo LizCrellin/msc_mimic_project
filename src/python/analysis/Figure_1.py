a57ff22c63c2fe1373461a480c4671f2b5d5c9e7
Liz_Crellin <ecrell01@student.bbk.ac.uk>
2026-08-22 18:55:09 +0100

    Recategorized admission type and admission location

--- src/python/analysis/Figure_1.py
+++ src/python/analysis/Figure_1.py
@@ -72,8 +72,42 @@ def categorize_ethnicity(ethnicity):
     return ethnicity
 
 icu_stays['ethnicity'] = icu_stays['race'].apply(categorize_ethnicity)
-print(sorted(icu_stays['race'].unique(), key=str))
-print(sorted(icu_stays['ethnicity'].unique(), key=str))
+# print(sorted(icu_stays['race'].unique(), key=str))
+# print(sorted(icu_stays['ethnicity'].unique(), key=str))
+
+# Admission type
+def categorize_adm_type(admission_type):
+    if 'ELECTIVE' in admission_type or 'SURGICAL SAME DAY' in admission_type:
+        admission_type = 'ELECTIVE'
+    elif 'URGENT' in admission_type:
+        admission_type = 'URGENT'
+    elif 'DIRECT EMER' in admission_type or 'EW EMER' in admission_type:
+        admission_type = 'EMERGENCY'
+    elif 'OBSERVATION' in admission_type:
+        admission_type = 'OBSERVATION'
+    else: 
+        admission_type = 'OTHER'
+    return admission_type
+
+icu_stays['admission_type'] = icu_stays['admission_type'].apply(categorize_adm_type)
+
+# Admission location
+def categorize_adm_loc(admission_location):
+    if 'EMERGENCY' in admission_location:
+        admission_location = 'Emergency room'
+    elif 'PHYSICIAN REFERRAL' in admission_location or 'CLINIC REFERRAL' in admission_location:
+        admission_location = 'Referral'
+    elif 'WALK-IN/SELF REFERRAL' in admission_location:
+        admission_location = 'Self-referral'
+    elif 'TRANSFER FROM HOSPITAL' in admission_location or 'TRANSFER FROM SKILLED NURSING' in admission_location:
+        admission_location = 'Transfer'
+    elif 'AMBULATORY SURGERY TRANSFER' in admission_location or 'PACU' in admission_location or 'PROCEDURE SITE' in admission_location:
+        admission_location = 'Procedure'
+    else: 
+        admission_location = 'Unknown'
+    return admission_location
+
+icu_stays['admission_location'] = icu_stays['admission_location'].apply(categorize_adm_loc)
 
 
 # Two groups, LOS >= 7 and the rest
